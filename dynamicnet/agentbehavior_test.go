b3d5db487d6d8e7245c6a3899a22ce9f5132d452
GaudiestTooth17 <alex.r.anthon@gmail.com>
2020-11-05 18:17:14 -0700

    commented out behavioral test as there is currently no dynamism in networks

--- dynamicnet/agentbehavior_test.go
+++ dynamicnet/agentbehavior_test.go
@@ -1,39 +1,41 @@
 package dynamicnet
 
-import (
-	"testing"
+// There is currently no dynamic behavior in networks, so this test has no bearing
 
-	"github.com/GaudiestTooth17/infection-resistant-network/diseasednetwork"
-	"github.com/GaudiestTooth17/infection-resistant-network/networkgenerator"
-)
+// import (
+// 	"testing"
 
-const (
-	stateS = diseasednetwork.StateS
-	stateE = diseasednetwork.StateE
-	stateI = diseasednetwork.StateI
-	stateR = diseasednetwork.StateR
-)
+// 	"github.com/GaudiestTooth17/infection-resistant-network/diseasednetwork"
+// 	"github.com/GaudiestTooth17/infection-resistant-network/networkgenerator"
+// )
 
-func TestImmuneNetwork(t *testing.T) {
-	numNodes := 100
-	net := diseasednetwork.NewDiseasedNetwork(
-		[]diseasednetwork.Disease{diseasednetwork.NewBasicDisease(1, 1, 1.0, diseasednetwork.NewInfectN(1))},
-		networkgenerator.MakeCompleteNetwork(numNodes))
+// const (
+// 	stateS = diseasednetwork.StateS
+// 	stateE = diseasednetwork.StateE
+// 	stateI = diseasednetwork.StateI
+// 	stateR = diseasednetwork.StateR
+// )
 
-	numInfected := len(net.FindNodesInState(stateI, 0))
-	if numInfected != 1 {
-		t.Errorf("Expected 1 infected node to start with, found %d", numInfected)
-	}
+// func TestImmuneNetwork(t *testing.T) {
+// 	numNodes := 100
+// 	net := diseasednetwork.NewDiseasedNetwork(
+// 		[]diseasednetwork.Disease{diseasednetwork.NewBasicDisease(1, 1, 1.0, diseasednetwork.NewInfectN(1))},
+// 		networkgenerator.MakeCompleteNetwork(numNodes))
 
-	for i := 0; i < 20; i++ {
-		net.Step()
-		numSusceptible := len(net.FindNodesInState(stateS, 0))
-		if numSusceptible != numNodes-1 {
-			numExposed := len(net.FindNodesInState(stateE, 0))
-			numInfected := len(net.FindNodesInState(stateI, 0))
-			numRecovered := len(net.FindNodesInState(stateR, 0))
-			t.Errorf("(step %d) Expected %d susceptible nodes, found %d.\nAlso found %d exposed, %d infected, %d recovered.\n",
-				i, numNodes-1, numSusceptible, numExposed, numInfected, numRecovered)
-		}
-	}
-}
+// 	numInfected := len(net.FindNodesInState(stateI, 0))
+// 	if numInfected != 1 {
+// 		t.Errorf("Expected 1 infected node to start with, found %d", numInfected)
+// 	}
+
+// 	for i := 0; i < 20; i++ {
+// 		net.Step()
+// 		numSusceptible := len(net.FindNodesInState(stateS, 0))
+// 		if numSusceptible != numNodes-1 {
+// 			numExposed := len(net.FindNodesInState(stateE, 0))
+// 			numInfected := len(net.FindNodesInState(stateI, 0))
+// 			numRecovered := len(net.FindNodesInState(stateR, 0))
+// 			t.Errorf("(step %d) Expected %d susceptible nodes, found %d.\nAlso found %d exposed, %d infected, %d recovered.\n",
+// 				i, numNodes-1, numSusceptible, numExposed, numInfected, numRecovered)
+// 		}
+// 	}
+// }
