d45ae798891cb888c422dc1cc3ae700154974136
GaudiestTooth17 <alex.r.anthon@gmail.com>
2020-12-07 22:28:51 -0700

    networkgen.py now outputs enough info for graph-visualizer to display the graphs

--- networkgen.py
+++ networkgen.py
@@ -82,6 +82,11 @@ def output_graph(G: nx.Graph):
     print(max(G.nodes()) + 1)
     for e in G.edges():
         print(f'{e[0]} {e[1]}')
+    # this code is just for the visualization program I made ("graph-visualizer")
+    print('\n')
+    layout = nx.spring_layout(G)
+    for id, coordinate in layout.items():
+        print(f'{id} {coordinate[0]} {coordinate[1]}')
 
 
 if __name__ == '__main__':
